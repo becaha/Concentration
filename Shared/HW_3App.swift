f54432828167403704240ffe95929f0dce792786
Rebecca Haggard <beccahagg5@gmail.com>
2020-09-08 11:24:52 -0600

    Initial Commit

---
+++ Shared/HW_3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_3App.swift
+//  Shared
+//
+//  Created by Rebecca Nybo on 9/8/20.
+//
+
+import SwiftUI
+
+@main
+struct HW_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
